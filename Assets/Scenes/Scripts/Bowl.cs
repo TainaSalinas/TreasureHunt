77ce225685fb9f280d9ca58f3b01f23afc5a4ae1
TainaSalinas <TainaSalinas@users.noreply.github.com>
2022-09-27 12:22:52 -0400

    Last trigger scripts?
    For the ending interaction

---
+++ Assets/Scenes/Scripts/Bowl.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bowl : MonoBehaviour
+{
+
+    private bool positive;
+    public GameObject purpleball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "character")
+        {
+            positive = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (positive == true && Input.GetKeyDown(KeyCode.E))
+        {
+
+            purpleball.SetActive(true);
+        }
+    }
+}
