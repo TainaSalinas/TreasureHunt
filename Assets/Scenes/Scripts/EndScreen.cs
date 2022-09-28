4cd8ace6ee6918a34d554f1d3909747adb946fb4
TainaSalinas <TainaSalinas@users.noreply.github.com>
2022-09-27 21:02:23 -0400

    Interior
    more interior items, purely aesthetic

--- Assets/Scenes/Scripts/EndScreen.cs
+++ Assets/Scenes/Scripts/EndScreen.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class EndScreen : MonoBehaviour
 {
+    public bool positive;
+    public GameObject purpleball;
+
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "bowlingball")
+        {
+            positive = true;
+        }
     }
 
     // Update is called once per frame
